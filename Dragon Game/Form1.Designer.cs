4b347c823e6a87a761e892b2936d42c2ad0efde4
Olson <wayne.olson@austin.k12.mn.us>
2020-05-14 15:20:03 -0500

    Add project files.

---
+++ Dragon Game/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Dragon_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playground = new System.Windows.Forms.Panel();
+            this.Pic_Missle4 = new System.Windows.Forms.PictureBox();
+            this.Pic_Missle3 = new System.Windows.Forms.PictureBox();
+            this.PicMissle2 = new System.Windows.Forms.PictureBox();
+            this.PicMissle = new System.Windows.Forms.PictureBox();
+            this.lbl_Endgame = new System.Windows.Forms.Label();
+            this.Pic_Ring3 = new System.Windows.Forms.PictureBox();
+            this.Pic_Ring4 = new System.Windows.Forms.PictureBox();
+            this.Pic_Ring2 = new System.Windows.Forms.PictureBox();
+            this.Pic_Ring1 = new System.Windows.Forms.PictureBox();
+            this.lbl_Score = new System.Windows.Forms.Label();
+            this.Pic_Dragon = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tmrMissle = new System.Windows.Forms.Timer(this.components);
+            this.tmrMissle2 = new System.Windows.Forms.Timer(this.components);
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Missle4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Missle3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicMissle2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicMissle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Ring3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Ring4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Ring2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Ring1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Dragon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playground
+            // 
+            this.playground.Controls.Add(this.Pic_Missle4);
+            this.playground.Controls.Add(this.Pic_Missle3);
+            this.playground.Controls.Add(this.PicMissle2);
+            this.playground.Controls.Add(this.PicMissle);
+            this.playground.Controls.Add(this.lbl_Endgame);
+            this.playground.Controls.Add(this.Pic_Ring3);
+            this.playground.Controls.Add(this.Pic_Ring4);
+            this.playground.Controls.Add(this.Pic_Ring2);
+            this.playground.Controls.Add(this.Pic_Ring1);
+            this.playground.Controls.Add(this.lbl_Score);
+            this.playground.Controls.Add(this.Pic_Dragon);
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(800, 450);
+            this.playground.TabIndex = 0;
+            this.playground.Paint += new System.Windows.Forms.PaintEventHandler(this.Playground_Paint);
+            // 
+            // Pic_Missle4
+            // 
+            this.Pic_Missle4.BackColor = System.Drawing.Color.Transparent;
+            this.Pic_Missle4.Image = global::Dragon_Game.Properties.Resources.Missle;
+            this.Pic_Missle4.Location = new System.Drawing.Point(615, 193);
+            this.Pic_Missle4.Name = "Pic_Missle4";
+            this.Pic_Missle4.Size = new System.Drawing.Size(37, 81);
+            this.Pic_Missle4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pic_Missle4.TabIndex = 10;
+            this.Pic_Missle4.TabStop = false;
+            // 
+            // Pic_Missle3
+            // 
+            this.Pic_Missle3.BackColor = System.Drawing.Color.Transparent;
+            this.Pic_Missle3.Image = global::Dragon_Game.Properties.Resources.Missle;
+            this.Pic_Missle3.Location = new System.Drawing.Point(528, 193);
+            this.Pic_Missle3.Name = "Pic_Missle3";
+            this.Pic_Missle3.Size = new System.Drawing.Size(37, 81);
+            this.Pic_Missle3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pic_Missle3.TabIndex = 9;
+            this.Pic_Missle3.TabStop = false;
+            // 
+            // PicMissle2
+            // 
+            this.PicMissle2.BackColor = System.Drawing.Color.Transparent;
+            this.PicMissle2.Image = global::Dragon_Game.Properties.Resources.Missle;
+            this.PicMissle2.Location = new System.Drawing.Point(382, 185);
+            this.PicMissle2.Name = "PicMissle2";
+            this.PicMissle2.Size = new System.Drawing.Size(37, 81);
+            this.PicMissle2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PicMissle2.TabIndex = 8;
+            this.PicMissle2.TabStop = false;
+            // 
+            // PicMissle
+            // 
+            this.PicMissle.BackColor = System.Drawing.Color.Transparent;
+            this.PicMissle.Image = global::Dragon_Game.Properties.Resources.Missle;
+            this.PicMissle.Location = new System.Drawing.Point(461, 357);
+            this.PicMissle.Name = "PicMissle";
+            this.PicMissle.Size = new System.Drawing.Size(37, 81);
+            this.PicMissle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PicMissle.TabIndex = 7;
+            this.PicMissle.TabStop = false;
+            // 
+            // lbl_Endgame
+            // 
+            this.lbl_Endgame.AutoSize = true;
+            this.lbl_Endgame.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Endgame.Location = new System.Drawing.Point(77, 278);
+            this.lbl_Endgame.Name = "lbl_Endgame";
+            this.lbl_Endgame.Size = new System.Drawing.Size(207, 125);
+            this.lbl_Endgame.TabIndex = 6;
+            this.lbl_Endgame.Text = "Game Over\r\n\r\n\r\nPress F1 to restart\r\nPress escape to exit";
+            // 
+            // Pic_Ring3
+            // 
+            this.Pic_Ring3.Image = ((System.Drawing.Image)(resources.GetObject("Pic_Ring3.Image")));
+            this.Pic_Ring3.Location = new System.Drawing.Point(669, 117);
+            this.Pic_Ring3.Name = "Pic_Ring3";
+            this.Pic_Ring3.Size = new System.Drawing.Size(58, 52);
+            this.Pic_Ring3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pic_Ring3.TabIndex = 5;
+            this.Pic_Ring3.TabStop = false;
+            // 
+            // Pic_Ring4
+            // 
+            this.Pic_Ring4.Image = ((System.Drawing.Image)(resources.GetObject("Pic_Ring4.Image")));
+            this.Pic_Ring4.Location = new System.Drawing.Point(279, 73);
+            this.Pic_Ring4.Name = "Pic_Ring4";
+            this.Pic_Ring4.Size = new System.Drawing.Size(58, 52);
+            this.Pic_Ring4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pic_Ring4.TabIndex = 4;
+            this.Pic_Ring4.TabStop = false;
+            // 
+            // Pic_Ring2
+            // 
+            this.Pic_Ring2.Image = ((System.Drawing.Image)(resources.GetObject("Pic_Ring2.Image")));
+            this.Pic_Ring2.Location = new System.Drawing.Point(362, 117);
+            this.Pic_Ring2.Name = "Pic_Ring2";
+            this.Pic_Ring2.Size = new System.Drawing.Size(58, 52);
+            this.Pic_Ring2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pic_Ring2.TabIndex = 3;
+            this.Pic_Ring2.TabStop = false;
+            // 
+            // Pic_Ring1
+            // 
+            this.Pic_Ring1.Image = ((System.Drawing.Image)(resources.GetObject("Pic_Ring1.Image")));
+            this.Pic_Ring1.Location = new System.Drawing.Point(507, 73);
+            this.Pic_Ring1.Name = "Pic_Ring1";
+            this.Pic_Ring1.Size = new System.Drawing.Size(58, 52);
+            this.Pic_Ring1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pic_Ring1.TabIndex = 2;
+            this.Pic_Ring1.TabStop = false;
+            // 
+            // lbl_Score
+            // 
+            this.lbl_Score.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lbl_Score.Location = new System.Drawing.Point(37, 11);
+            this.lbl_Score.Name = "lbl_Score";
+            this.lbl_Score.Size = new System.Drawing.Size(53, 17);
+            this.lbl_Score.TabIndex = 1;
+            this.lbl_Score.Text = "0";
+            // 
+            // Pic_Dragon
+            // 
+            this.Pic_Dragon.BackColor = System.Drawing.Color.Transparent;
+            this.Pic_Dragon.Image = ((System.Drawing.Image)(resources.GetObject("Pic_Dragon.Image")));
+            this.Pic_Dragon.Location = new System.Drawing.Point(124, 74);
+            this.Pic_Dragon.Name = "Pic_Dragon";
+            this.Pic_Dragon.Size = new System.Drawing.Size(113, 95);
+            this.Pic_Dragon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pic_Dragon.TabIndex = 0;
+            this.Pic_Dragon.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // tmrMissle
+            // 
+            this.tmrMissle.Enabled = true;
+            this.tmrMissle.Interval = 1;
+            this.tmrMissle.Tick += new System.EventHandler(this.TmrMissle_Tick);
+            // 
+            // tmrMissle2
+            // 
+            this.tmrMissle2.Interval = 1;
+            this.tmrMissle2.Tick += new System.EventHandler(this.TmrMissle2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.playground);
+            this.Name = "Form1";
+            this.Text = "Dragon Game Mr. Olson";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.playground.ResumeLayout(false);
+            this.playground.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Missle4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Missle3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicMissle2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicMissle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Ring3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Ring4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Ring2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Ring1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Dragon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.PictureBox Pic_Dragon;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox Pic_Ring1;
+        private System.Windows.Forms.Label lbl_Score;
+        private System.Windows.Forms.PictureBox Pic_Ring3;
+        private System.Windows.Forms.PictureBox Pic_Ring4;
+        private System.Windows.Forms.PictureBox Pic_Ring2;
+        private System.Windows.Forms.Label lbl_Endgame;
+        private System.Windows.Forms.PictureBox PicMissle;
+        private System.Windows.Forms.Timer tmrMissle;
+        private System.Windows.Forms.PictureBox Pic_Missle4;
+        private System.Windows.Forms.PictureBox Pic_Missle3;
+        private System.Windows.Forms.PictureBox PicMissle2;
+        private System.Windows.Forms.Timer tmrMissle2;
+    }
+}
+
---
+++ Dragon Game/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;   // Add this to your program
+
+
+namespace Dragon_Game
+{
+
+    // Mr. Olson Dragon Game
+
+
+    public partial class Form1 : Form
+    {
+       private SoundPlayer  _soundplayer;  // add a new sound player 
+
+
+        int score = 0;
+        PictureBox[] rings = new PictureBox[4];
+        PictureBox[] Missles = new PictureBox[4];
+
+        Random r = new Random();
+
+        public void Restart()
+        {
+           
+            r.Next(1,100);
+            ringstart();
+
+            lbl_Endgame.Hide();
+            score = 0;
+            lbl_Score.Text = "0"; 
+            Missles[0].Visible=true;
+            Missles[0].Left =playground.Left+20;
+            Missles[0].Top = playground.Height - 50;
+            timer1.Enabled = true;
+            timer1.Interval = 10;
+            tmrMissle.Enabled = true;
+
+        }
+        public void ringstart()
+        {
+            rings[0] = Pic_Ring1;
+            rings[1] = Pic_Ring2;
+            rings[2] = Pic_Ring3;
+            rings[3] = Pic_Ring4;
+
+            rings[0].Top = r.Next(40, playground.Height - 40);
+            rings[0].Left = r.Next(40, playground.Width - 40);
+
+            rings[1].Top = r.Next(40, playground.Height - 40);
+            rings[1].Left = r.Next(40, playground.Width - 40);
+
+            rings[2].Top = r.Next(40, playground.Height - 40);
+            rings[2].Left = r.Next(40, playground.Width - 40);
+
+            rings[3].Top = r.Next(40, playground.Height - 40);
+            rings[3].Left = r.Next(40, playground.Width - 40);
+
+           
+
+            
+            for (int i = 0; i < 4; i++)
+            {
+                rings[i].Visible = true;
+            }
+
+
+            Missles[0] = PicMissle;
+            Missles[1] = PicMissle2;
+            Missles[2] = Pic_Missle3;
+            Missles[3] = Pic_Missle4;
+
+            for (int i = 0; i < 4; i++)
+            {
+                Missles[i].Visible = false;
+            }
+        }
+
+
+        public void Edgame()
+        {
+            lbl_Endgame.Show();
+       
+        }
+
+        public Form1()
+        {
+            _soundplayer = new SoundPlayer(Dragon_Game .Properties.Resources.Missile_1);  // create the new sound player
+            InitializeComponent();
+            this.TopMost = true;                            // This will bring the form to the front/panel
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+            Restart();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Restart();
+           
+        }
+       
+      
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+
+                if (Pic_Dragon.Bounds.IntersectsWith(rings[i].Bounds) && rings[i].Visible)
+                {
+                    rings[i].Hide();
+                    score = score + 1;
+                    lbl_Score.Text = score.ToString();
+
+                }
+            }
+
+            Pic_Dragon.Left += 2;
+
+
+            if (Pic_Dragon.Right>= playground.Right)
+            {
+                Pic_Dragon.Left = 0;
+                
+            }
+
+
+            for(int i=0;i<4;i++)
+            {
+                if (Missles[i].Bounds.IntersectsWith(Pic_Dragon.Bounds) && Missles[i].Visible)
+                {
+                    Edgame();
+                    timer1.Enabled = false;
+                    tmrMissle.Enabled = false;
+                    tmrMissle2.Enabled = false;
+                    ringstart();
+                    
+                   
+                    
+                }
+            }
+            
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Down)
+            {
+                Pic_Dragon.Top += 3;
+            }
+           else if(e.KeyCode == Keys.Up)
+                {
+                Pic_Dragon.Top -= 3;
+            }
+            else if (e.KeyCode== Keys.Left)
+            {
+                Pic_Dragon.Left -= 3;
+            }
+
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+
+            if (e.KeyCode == Keys.F1)
+            {
+                Restart();
+                
+            }
+        }
+
+        private void TmrMissle_Tick(object sender, EventArgs e)
+        {
+            // part 5
+
+            Missles[0].Top -= 5;
+            
+         
+            
+            if(Missles[0].Top<0)
+            {
+               
+                Missles[0].Top = playground.Height - 50;
+                Missles[0].Left = r.Next(20, playground.Width - 30);
+                _soundplayer.Play();   // play the sound
+            }
+
+            if (score==2)
+            {
+                ringstart();
+                tmrMissle2.Enabled = true;
+                Missles[0].Visible = true;
+                Missles[1].Visible = true;
+            }
+           
+        }
+
+        private void TmrMissle2_Tick(object sender, EventArgs e)
+        {
+            Missles[1].Top -= 5;
+            
+
+
+            if (Missles[1].Top < 0)
+            {
+
+                Missles[1].Top = playground.Height - 50;
+                Missles[1].Left = r.Next(20, playground.Width - 30);
+            }
+
+        }
+
+        private void Playground_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+    
+}
